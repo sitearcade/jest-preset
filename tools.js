ee819a52886f1639b68c91b3614c06943a90b500
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-10-09 11:23:21 -0500

    Add mass tools export

---
+++ tools.js
@@ -0,0 +1,7 @@
+// export
+
+export * from '@testing-library/dom';
+export * from '@testing-library/jest-dom';
+export * from '@testing-library/react';
+export * from '@testing-library/react-hooks';
+export * from '@testing-library/user-event';
