6baa9a3dd7da1d54a6e6882d6656246276f6565f
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-10-09 12:40:48 -0500

    add delay helper

--- tools.js
+++ tools.js
@@ -1,3 +1,12 @@
+// import
+
+const {waitFor} = require('@testing-library/dom');
+
+// fns
+
+const waitForTimeout = (ms) =>
+  waitFor(() => new Promise((resolve) => setTimeout(resolve, ms)));
+
 // export
 
 module.exports = {
@@ -6,4 +15,5 @@ module.exports = {
   ...require('@testing-library/react'),
   ...require('@testing-library/react-hooks'),
   ...require('@testing-library/user-event'),
+  waitForTimeout,
 };
