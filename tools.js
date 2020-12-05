b2c20c579f3fad76398eb80ae238db359ef765f8
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-12-05 10:13:05 -0600

    Allow no-react installed

--- tools.js
+++ tools.js
@@ -7,6 +7,15 @@ const {waitFor} = require('@testing-library/dom');
 const waitForTimeout = (ms) =>
   waitFor(() => new Promise((resolve) => setTimeout(resolve, ms)));
 
+// config
+
+let hasReact = false;
+
+try {
+  // eslint-disable-next-line import/no-extraneous-dependencies
+  hasReact = require('react/package.json');
+} catch {}
+
 // export
 
 module.exports = {
@@ -15,8 +24,10 @@ module.exports = {
   ...require('jest-mock-proxy'),
   ...require('@testing-library/dom'),
   ...require('@testing-library/jest-dom'),
-  ...require('@testing-library/react'),
-  ...require('@testing-library/react-hooks'),
   ...require('@testing-library/user-event'),
+  ...(hasReact ? {
+    ...require('@testing-library/react'),
+    ...require('@testing-library/react-hooks'),
+  } : {}),
   waitForTimeout,
 };
