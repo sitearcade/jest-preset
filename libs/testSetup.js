25d29da96fa2e8de1001a57de937efa4b6abd516
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-12-05 10:31:39 -0600

    auto-import react

--- libs/testSetup.js
+++ libs/testSetup.js
@@ -2,3 +2,7 @@
 
 // eslint-disable-next-line fp/no-mutating-methods
 Object.defineProperty(window, 'scrollTo', {value: () => ({}), writable: true});
+
+try {
+  window.React = require('react');
+} catch {}
