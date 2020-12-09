00373d66ed104b3d335e99e4a4b7313ab0e1511f
Mike Stop Continues <mikestopcontinues@gmail.com>
2020-12-09 09:59:22 -0600

    Bundle date and proxy mocks

--- tools.js
+++ tools.js
@@ -21,14 +21,17 @@ try {
 module.exports = {
   fetchMock: require('jest-fetch-mock'),
   matchMediaMock: new MatchMediaMock(),
-  ...require('jest-date-mock'),
-  ...require('jest-mock-proxy'),
+  dateMock: require('jest-date-mock'),
+  proxyMock: require('jest-mock-proxy'),
+
   ...require('@testing-library/dom'),
   ...require('@testing-library/jest-dom'),
   ...require('@testing-library/user-event'),
+
   ...(hasReact ? {
     ...require('@testing-library/react'),
     ...require('@testing-library/react-hooks'),
   } : {}),
+
   waitForTimeout,
 };
